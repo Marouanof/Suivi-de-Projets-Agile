bf8c5c1b9b76a5ca08b5ec7ec787d1a989dcb9d6
Marouane <merouanefaik@gmail.com>
2026-01-21 15:56:51 +0100

    Testeur : Backend maj3

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,17 +1,23 @@
 const db = require("../config/database");
+const bcrypt = require("bcrypt");
 
 exports.findByEmail = (email) => {
     return db.query("SELECT * FROM users WHERE email = ?", [email]);
 };
 
-exports.create = (user) => {
+exports.create = async (user) => {
     const { id, email, password, first_name, last_name, role } = user;
     if (!email || !password) {
         return Promise.reject(new Error("Email and password are required"));
     }
+
     const normalizedRole = role || "USER";
+    const hashedPassword = password.startsWith("$2")
+        ? password
+        : await bcrypt.hash(password, 10);
+
     return db.query(
         "INSERT INTO users (id, email, password, first_name, last_name, role) VALUES (?, ?, ?, ?, ?, ?)",
-        [id, email, password, first_name, last_name, normalizedRole]
+        [id, email, hashedPassword, first_name, last_name, normalizedRole]
     );
 };
